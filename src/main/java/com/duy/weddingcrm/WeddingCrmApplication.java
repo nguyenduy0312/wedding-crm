cb49ed657d01dc7e14c6b90194eab6c698113e91
nguyenduy0312 <160990102+nguyenduy0312@users.noreply.github.com>
2026-02-26 17:40:34 +0700

    Du an WeddingCrm

---
+++ src/main/java/com/duy/weddingcrm/WeddingCrmApplication.java
@@ -0,0 +1,13 @@
+package com.duy.weddingcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duy/weddingcrm/WeddingCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duy.weddingcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
